a3f09053a927edb7cf077f6ea9c2393f0b1d52e4
sumo-slonik <61011609+sumo-slonik@users.noreply.github.com>
2020-07-28 00:08:34 +0200

    Add files via upload
    działające rysowanie równi po wpisaniu wysokości i kąta

--- rownia/sketch.js
+++ rownia/sketch.js
@@ -1,12 +1,18 @@
 let confirmedHeight = false;
-let height = 100;
-
+let height = 0;
+let angle = 90;
+function degrees_to_radians(degrees)
+{
+    var pi = Math.PI;
+    return degrees * (pi/180);
+}
 function setup() {
     myCanvas = createCanvas(1100, 600);
     myCanvas.parent('main');
     img = loadImage('img/wykres.png');
     frameRate(100);
     beginShape();
+    noFill();
 }
 
 function Brick(x, y, mass) {
@@ -19,17 +25,39 @@ function Brick(x, y, mass) {
 Brick.prototype.display = function () {
     square(this.x, this.y, this.mass*10);
 }
-
+let kostka = new Brick(10,10,10);
 function draw() {
     strokeWeight(10);
     background(img);
     point(104, 500);
     point(104,500-height);
+    point(104+1/Math.tan(degrees_to_radians(angle))*height,500);
+    strokeWeight(4);
+    triangle(104, 500,104,500-height,104+1/Math.tan(degrees_to_radians(angle))*height,500);
 
 }
 
 function confirm_h() {
     height = document.getElementById('height').value;
+    height = height.replace(",",".");
+    let reg = /\d+(\.\d+)?/;
+    if(reg.test(height) && height > 0 && height <= 5){
+        document.getElementById('info_heigth').style.color = 'black';
+    }
+    else{
+        document.getElementById('info_heigth').style.color = 'red';
+        return;
+    }
     height *=  97;
-
+}
+function confirm_a() {
+    angle = document.getElementById("angle").value;
+    angle = angle.replace(",",".");
+    if(reg.test(height) && height > 0 && height <= 5){
+        document.getElementById('info_angle').style.color = 'black';
+    }
+    else{
+        document.getElementById('info_angle').style.color = 'red';
+        return;
+    }
 }
