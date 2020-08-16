e36e014ea5f2d30edd7e6c648b00aafbbcbb8fe2
kingasakol <68615103+kingasakol@users.noreply.github.com>
2020-08-16 13:38:05 +0200

    zderzenia sprezyste

--- zderzenia/sketch.js
+++ zderzenia/sketch.js
@@ -2,21 +2,23 @@ let velocity1 = 0;
 let velocity2 = 0;
 let weight1 = 0;
 let weight2 = 0;
-let c_w;
-let c_h;
+let c_w; // szerokość canvasu
+let c_h; // wysokość canvasu
 let tab;
-let z = 0; //położenie pierwszej kulki
-let w = 0; // położenie drugiej kulki
+let z = 0; // położenie 1-szej kulki
+let w = 0; // położenie 2-giej kulki
+let r = 0; // położenie dla złączonych kulek
 let vFirstBall = 1; //potrzebne do animacji
 let vSecondBall = 1; //potrzebne do animacji
 let vThirdBall = 0;
 let sizeFirstBall = 0.15; //potrzebne do animacji
 let sizeSecondBall = 0.15; //potrzebne do animacji
 let czySprezyste = true; //wartość początkowa oraz domyślne ustawienie
-let start = false;
+let start = false; //czy animacja działa
 let result = 0;
-let moveSecond = false;
-let firstTime = true;
+let moveSecond = false; //czy 2-ga kulka się porusza
+let moveTogether = false;
+let firstTime = true; //czy program wykonuje się po raz pierwszy
 
 
 window.addEventListener("resize", afterSize);
@@ -28,46 +30,56 @@ function change(){
         czySprezyste = true;
     } else if(myRadio.filter(":checked").val() == "Zderzenie_niesprezyste"){
         czySprezyste = false;
+        firstTime = true;
     }
+    restartAnimation();
     windowResized();
+    actualVNS();
 }
 
 //funkcja pobierająca wartości z pól input
 function getValue(idField) {
     var value = $("#" + idField).val();
+    value = parseInt(value, 10);
     if (isNaN(value)){
         alert("to nie liczba !!!");
         $("#" + idField).val("");
+        $('#start').prop("disabled", true);
     }
     else if(value == ""){
         alert("Puste pole !!!");
+        $('#start').prop("disabled", true);
     }
-    else {
+    else if (czySprezyste){
         if(idField == 'velocity1'){
             velocity1 = value;
             if(velocity1 != 0 && weight2 != 0 && weight1 != 0){
                 result = findMoment(velocity2, weight2, velocity1);
                 console.log("ug");
-                //result = a_momentum(weight1, weight2, velocity1, velocity2);
+                //result = aMomentum(weight1, weight2, velocity1, velocity2);
                 weight1 = result.toFixed(2);
                 $('#weight1').val(result);
-            } else if(velocity1 == 0) {
+            } else if(velocity1 == 0 && weight1 != 0 && weight2 != 0) {
                 velocity1 = findMoment(velocity2, weight2, weight1);
             }
         }
         else if(idField == 'velocity2'){
+            velocity2 = value;
             if(velocity2 == 0){
                 alert("Podaj wagę 2-giej kulki");
                 $('#wButton2').prop("disabled", false);
                 $('#weight2').prop("disabled", false);
                 velocity2 = value;
-            } else if(velocity2 != 0 && weight2 != 0){
+            } else if(velocity2 != 0 && weight2 != 0 && velocity1 != 0){
                 velocity2 = value;
                // result = findMoment(velocity1, weight1, velocity2);
-                //result = a_momentum(weight1, weight2, velocity1, velocity2);
+                //result = aMomentum(weight1, weight2, velocity1, velocity2);
                 result = findMoment(velocity1, weight1, velocity2).toFixed(2);
                 weight2 = result;
                 $('#weight2').val(result);
+            } else if(velocity2 != 0 && weight2 == 0 && (velocity1 != 0 || weight1 != 0)){
+                weight2 = findMoment(velocity1, weight1, velocity2).toFixed(2);
+                $('#weight2').val(weight2);
             }
 
         }
@@ -83,30 +95,143 @@ function getValue(idField) {
         }
         else if(idField == 'weight2'){
             weight2 = value;
-            if(weight2 != 0 && velocity2 != 0){
+            if(weight2 != 0 && (velocity2 != 0 || velocity2 != 0.00)){
                 velocity2 = findMoment(velocity1, weight1, weight2).toFixed(2);
                 $('#velocity2').val(velocity2);
-            } else if (weight2 != 0 && velocity2 == 0){
+                console.log("velocity2" + velocity2);
+            } else if (weight2 != 0 && velocity2 == 0 && velocity1 != 0 ){
                 console.log("jestem");
                 velocity2 = findMoment(velocity1, weight1, weight2).toFixed(2);
-                result = a_momentum(weight1, weight2, velocity1, velocity2);
+                result = aMomentum(weight1, weight2, velocity1, velocity2);
                 console.log(weight1, weight2, velocity1, velocity2);
+            } else if (weight2 != 0 && (velocity1 == 0 || weight1 == 0)){
+                console.log("taka sytuacja");
+                weight2 = value;
+                velocity2 = $("#velocity2").val();
             } else {
-                result = a_momentum(weight1, weight2, velocity1, velocity2).toFixed(2);
+                result = aMomentum(weight1, weight2, velocity1, velocity2).toFixed(2);
+                console.log("here");
             }
 
             vSecondBall = hashVelocity(velocity2);
         }
-    }
-/*
-    console.log(velocity1, velocity2, weight1, weight2);
-    console.log(weight1 + " - waga1");
-    console.log(velocity1 + " " + velocity2);
+    } else {
+        //zderzenie niesprezyste
+        //do dokończenia
 
- */
+        if(idField == 'velocity1'){
+            velocity1 = value;
+            if(velocity1 != 0 && weight2 != 0 && weight1 != 0){
+                if(velocity1 - velocity2 == 0 || velocity1 < velocity2){
+                    alert("Prędkość złączonych kulek musi być mniejsza niż prędkość pierwszej kulki !!!");
+                    $('#velocity1').val("");
+                    $('#start').prop("disabled", true);
+                } else {
+                    result = findMoment(velocity1 - velocity2, weight2, velocity2).toFixed(2);
+                    console.log("ug");
+                    //result = aMomentum(weight1, weight2, velocity1, velocity2);
+                    weight1 = result;
+                    $('#weight1').val(result);
+                    $('#start').prop("disabled", false);
+                }
+            } else if(velocity1 == 0) {
+                velocity1 = findMoment(velocity2, weight2, weight1);
+                $('#start').prop("disabled", false);
+            }
+        }
+        else if(idField == 'velocity2'){
+            velocity2 = value;
+            if(velocity2 == 0){
+                alert("Podaj wagę 2-giej kulki");
+                $('#wButton2').prop("disabled", false);
+                $('#weight2').prop("disabled", false);
+                velocity2 = value;
+            } else if(velocity2 != 0 && weight2 != 0 && velocity1 != 0){
+                velocity2 = value;
+                // result = findMoment(velocity1, weight1, velocity2);
+                //result = aMomentum(weight1, weight2, velocity1, velocity2);
+                if (velocity1 - velocity2 == 0){
+                    alert("Podaj inną wartość !!!");
+                    $('#weight2').val("");
+                    $('#start').prop("disabled", true);
+                } else if (velocity1 > velocity2){
+                    result = findMoment(velocity2, weight1, velocity1 - velocity2).toFixed(2);
+                    weight2 = result;
+                    $('#weight2').val(weight2);
+                    $('#start').prop("disabled", false);
+                } else {
+                    alert("Prędkość złączonych kulek musi być mniejsza niż prędkość pierwszej kulki !!!");
+                    $('#velocity2').val("");
+                    $('#start').prop("disabled", true);
+                }
+            } else if(velocity2 != 0 && weight2 == 0 && (velocity1 != 0 || weight1 != 0)){
+                if(velocity1 - velocity2 == 0){
+                    alert("Podaj inną wartość !!!");
+                    $('#weight2').val("");
+                    $('#start').prop("disabled", true);
+                } else if (velocity1 - velocity2 == 0){
+                    weight2 = findMoment(velocity2, weight1, velocity1 - velocity2).toFixed(2);
+                    $('#weight2').val(weight2);
+                    $('#start').prop("disabled", false);
+                } else {
+                    alert("Podaj inną wartość !!!");
+                    $('#weight2').val("");
+                    $('#start').prop("disabled", true);
+                }
+            }
+        }
+        else if(idField == 'weight1'){
+            weight1 = value;
+            $('#start').prop("disabled", false);
+            if (weight1 != 0 && velocity1 != 0 && weight2 != 0) {
+                result = findMoment(velocity2, weight1 + weight2, weight2);
+                velocity1 = result.toFixed(2);
+                $('#velocity1').val(velocity1);
+                $('#start').prop("disabled", false);
+            } else if(velocity1 != 0 && velocity2 != 0 && weight2 != 0){
+                weight1 = findMoment(velocity1 - velocity2, weight2, velocity2).toFixed(2);
+            }
+        }
+        else if(idField == 'weight2'){
+            console.log(weight2);
+            weight2 = value;
+            if(weight2 != 0 && (velocity2 != 0 || velocity2 != 0.00)){
+                velocity2 = findMoment(velocity1, weight1, weight1 + weight2).toFixed(2);
+                //Math.round(velocity2 * 100) / 100;
+                $('#velocity2').val(velocity2);
+                $('#start').prop("disabled", false);
+                console.log("velocity2" + velocity2);
+            } else if (weight2 != 0 && velocity2 == 0 && velocity1 != 0 && weight1 != 0){
+                //weight1 = parseInt(weight1, 10);
+                //weight2 = parseInt(weight2, 10);
+                suma = weight1 + weight2;
+                console.log("jestem" + suma);
+                velocity2 = findMoment(velocity1, weight1, weight1 + weight2).toFixed(2);
+                //result = aMomentum(weight1, weight2, velocity1, velocity2);
+                console.log(weight1, weight2, velocity1, velocity2);
+                $('#velocity2').val(velocity2);
+                $('#start').prop("disabled", false);
+            } else if (weight2 != 0 && (velocity1 == 0 || weight1 == 0)){
+                console.log("taka sytuacja");
+                weight2 = value;
+                velocity2 = $("#velocity2").val();
+            } else {
+                velocity2 = aMomentum(velocity1, weight1, weight1 + weight2);
+                console.log(velocity2 + "  z tego miejsca");
+                $('#velocity2').val(velocity2);
+                $('#start').prop("disabled", false);
+            }
+            vThirdBall = hashVelocity(velocity2);
+        }
+    }
     console.log(weight1, weight2, velocity1, velocity2 + "przed");
-    //var wynik = a_momentum(weight1, weight2, velocity1, velocity2);
+    //var wynik = aMomentum(weight1, weight2, velocity1, velocity2);
     actualVNS();
+    resizeCanvas(c_w, c_h);
+    background(230);
+    z = 0;
+    w = 0;
+    loop();
 }
 
 function actualVNS(){
@@ -116,15 +241,24 @@ function actualVNS(){
     }
     sizeFirstBall = hashSize(weight1);
     sizeSecondBall = hashSize(weight2);
-    //  windowResized();
-    //to jest do poprawienia
     vSecondBall = hashVelocity(velocity2);
+    if(!czySprezyste){
+        vThirdBall = hashVelocity(velocity2);
+        console.log(vThirdBall + "predkosci");
+    }
 }
 
 function firstLoop(){
-    if(firstTime == true){
-        console.log("dzialam");
-        var wynik = findMoment(velocity1, velocity2, weight2);
+    if(firstTime && czySprezyste){
+        var wynik = findMoment(velocity1, weight1, weight2);
+        result = wynik;
+        console.log("wynik " + wynik);
+        $('#velocity2').val(result);
+        firstTime = false;
+    } else if(firstTime && !czySprezyste){
+        console.log("w:" + velocity1, weight1, velocity2, weight2);
+        var temp = weight1 + weight2;
+        var wynik = velocity1 * weight1 / temp;
         result = wynik;
         console.log("wynik " + wynik);
         $('#velocity2').val(result);
@@ -133,12 +267,7 @@ function firstLoop(){
 }
 
 function findResult(){
-   /* if(result != undefined){
-        result = result.toFixed(2);
-        $('#velocity2').val(result);
-    }
-    */
-
+    firstLoop();
     vFirstBall = hashVelocity(velocity1);
     if(czySprezyste){
         vSecondBall = hashVelocity(velocity2);
@@ -147,23 +276,21 @@ function findResult(){
     sizeSecondBall = hashSize(weight2);
     $('#vButton2')[0].disabled = false;
     $('#velocity2')[0].disabled = false;
-  //  windowResized();
-    //to jest do poprawienia
     vSecondBall = hashVelocity(velocity2);
-
 }
 
 function afterLoad(){
     c_h = $('#canvas').height();
     c_w = $('#canvas').width();
-    if($('#vBUtton2').is(":disabled")){
-        $('#vButton2').prop('disabled', false);
-        $('#velocity2').prop('disabled', false);
-    } else {
-        $('#vButton2').prop('disabled', true);
-        $('#velocity2').prop('disabled', true);
+    if(firstTime){
+        if($('#vButton2').is(":disabled")){
+            $('#vButton2').prop('disabled', false);
+            $('#velocity2').prop('disabled', false);
+        } else {
+            $('#vButton2').prop('disabled', true);
+            $('#velocity2').prop('disabled', true);
+        }
     }
-
 }
 
 function afterSize(){
@@ -181,17 +308,21 @@ function draw(){
     background(230);
     c_w = Math.floor(c_w);
     c_h = Math.floor(c_h);
-
+    let c = color('rgb(0, 128, 255)');
+    let c2 = color('rgb(223, 32, 32)');
+    fill(c);
     if(!start && czySprezyste){
         //zatrzymana i sprezyste
         ellipse(width / 4 + z, height / 2, sizeFirstBall * c_h, sizeFirstBall * c_h);
         ellipse(width * 5 / 8 + w, height / 2, sizeSecondBall * c_h, sizeSecondBall * c_h);
     } else if(!start && !czySprezyste){
         //zatrzymana i niesprezyste
-        //trzeba wrocic
-        ellipse(width / 4 + z, height / 2, sizeFirstBall * c_h, sizeFirstBall * c_h);
-        ellipse(width * 5 / 8 + w, height / 2, sizeSecondBall * c_h, sizeSecondBall * c_h);
+        noStroke();
+        fill(c2);
+        ellipse(width / 4 + z + r, height / 2, sizeFirstBall * c_h, sizeFirstBall * c_h);
+        ellipse(width * 5 / 8 + r, height / 2, sizeSecondBall * c_h, sizeSecondBall * c_h);
     } else if(czySprezyste && start) {
+        //sprezyste i działa
         ellipse(width / 4 + z, height / 2, sizeFirstBall * c_h, sizeFirstBall * c_h);
         ellipse(width * 5 / 8 + w, height / 2, sizeSecondBall * c_h, sizeSecondBall * c_h);
 
@@ -221,21 +352,40 @@ function draw(){
         } else {
             z += vFirstBall;
         }
-    /*} else if(!czySprezyste && start){
-
+    } else if(!czySprezyste && start) {
         //niesprezyste
-        //trzeba wrocic
-        console.log("niesprezyste");
-        ellipse(width / 4, height / 2, 0.15 * c_h, 0.15 * c_h);
-        ellipse(width * 5 / 8 + vSecondBall, height / 2, 0.15 * c_h, 0.15 * c_h);
-        t = Math.round(c_w * 3 / 4 - 0.15 * c_h / 2);
-        if (vSecondBall == t) {
-            vSecondBall = 0;
+        fill(c2);
+        noStroke();
+        ellipse(width / 4 + z + r, height / 2, sizeFirstBall * c_h, sizeFirstBall * c_h);
+        ellipse(width * 5 / 8 + r, height / 2, sizeSecondBall * c_h, sizeSecondBall * c_h);
+
+        t1 = Math.round(c_w * 3 / 4 - sizeFirstBall * c_h / 2);
+        t2 = Math.round(c_w * 3 / 8 - sizeSecondBall * c_h / 2);
+
+        //warunek końca animacji
+        if (Math.round(z / vFirstBall) == Math.round(t1 / vFirstBall)) {
+            z = 0;
+            r = 0;
+            moveTogether = false;
             noLoop();
         }
-        vSecondBall++;
+        if(Math.round(r / vThirdBall) == Math.round(t2 / vThirdBall)){
+            z = 0;
+            r = 0;
+            moveTogether = false;
+            noLoop();
+        }
 
-         */
+        if(Math.round(width * 1 / 4 + z) == Math.round(width * 5 / 8 - sizeSecondBall * c_h / 2) ||
+                Math.round(width * 1 / 4 + z) == Math.round(width * 5 / 8 - sizeFirstBall * c_h / 2)){
+            moveTogether = true;
+        }
+
+        if(!moveTogether){
+            z += vFirstBall;
+        } else {
+            r += vThirdBall;
+        }
     }
 }
 
@@ -256,7 +406,7 @@ function findMoment(x1, x2, x3){
     return x1 * x2 / x3;
 }
 
-function a_momentum(w1, w2, v1, v2){
+function aMomentum(w1, w2, v1, v2){
     tab = new Array(w1, w2, v1, v2);
     x = 0;
 
@@ -267,7 +417,6 @@ function a_momentum(w1, w2, v1, v2){
     }
     if(x == 1){
         if(w1 == 0){
-            //tu uzupełnić poprawne wartości, które będą podawane po obliczeniu
             //weight1 = v2 * w2 / v1;
             return v2 * w2 / v1;
         } else if (w2 == 0){
@@ -298,6 +447,7 @@ function hashVelocity(v){
     return velocityValuesArray[x];
 }
 
+//funkcja haszująca wielkości kulek
 function hashSize(w){
     const weightValuesArray = new Array(0.04, 0.08, 0.12, 0.16, 0.2, 0.24, 0.28, 0.32, 0.36, 0.4);
     var y = 0;
@@ -310,14 +460,10 @@ function hashSize(w){
     }
     return weightValuesArray[y];
 }
+
 //funkcje do buttonów na stopce
 function playAnimation(){
     start = true;
-  /*
-    console.log(hashVelocity(velocity1) + "value from array");
-    console.log(hashVelocity(velocity2) + "value from array 2 ");
-    console.log(hashSize(weight1) + "value from array 3 ");
-   */
 }
 
 function pauseAnimation(){
@@ -328,4 +474,12 @@ function pauseAnimation(){
 function restartAnimation(){
     start = false;
     windowResized();
+    $('#velocity1').val("");
+    $('#velocity2').val("");
+    $('#weight1').val("");
+    $('#weight2').val("");
+    velocity1 = 0;
+    velocity2 = 0;
+    weight1 = 0;
+    weight2 = 0;
 }
